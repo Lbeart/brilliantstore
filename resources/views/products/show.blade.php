4c76de84ca392abc0df11bd3d60b8c60adb79047
Leart Bytyqi <leart.bytyqii98@gmail.com>
2026-02-22 15:43:49 +0100

    Remove wrong h1 from about page

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -936,9 +936,9 @@
 
            @php
 $folds = [
-    ['value'=>'fold1','name'=>'Fold 1 (1:2)','extra'=>0,'fullness'=>2,'img'=>'classic1.jpg'],
-    ['value'=>'fold2','name'=>'Fold 2 (1:2.5)','extra'=>0,'fullness'=>2.5,'img'=>'classic2.jpg'],
-    ['value'=>'fold3','name'=>'Fold 3 (1:3)','extra'=>0,'fullness'=>3,'img'=>'triple.jpg'],
+     ['value'=>'fold1','name'=>'Fold 1 (1:2)','ratio'=>2,'img'=>'classic1.jpg'],
+    ['value'=>'fold2','name'=>'Fold 2 (1:2.5)','ratio'=>2.5,'img'=>'classic2.jpg'],
+    ['value'=>'fold3','name'=>'Fold 3 (1:3)','ratio'=>3,'img'=>'triple.jpg'],
     ['value'=>'grommet','name'=>'Grommet','extra'=>1.5,'fullness'=>2,'img'=>'grommet.jpg'],
     ['value'=>'pencil','name'=>'Pencil Pleat','extra'=>1,'fullness'=>2.5,'img'=>'pencil.jpg'],
     ['value'=>'swave','name'=>'S-Wave','extra'=>2,'fullness'=>2.2,'img'=>'swave.jpg'],
