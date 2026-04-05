70e00ecf703b35b2bdcdf08d5adb0a2530bc5ca1
Lirie Bytyqi <leart.bytyqii98@gmail.com>
2026-04-05 22:53:34 +0200

    ...

--- resources/views/track/show.blade.php
+++ resources/views/track/show.blade.php
@@ -258,11 +258,7 @@
         </button>
 
         <!-- PDF -->
-        <a href="{{ route('orders.invoice.public', $order->id) }}" 
-           class="btn btn-danger" target="_blank">
-            <i class="bi bi-file-earmark-pdf"></i> Shkarko PDF
-        </a>
-
+        
     </div>
 
 </div>
