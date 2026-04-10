81921d6ff1964bb9038f6c895700023dc0556e36
Lirie Bytyqi <leart.bytyqii98@gmail.com>
2026-04-10 10:27:16 +0200

    ...

---
+++ resources/views/sitemap.blade.php
@@ -0,0 +1,101 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+
+<!-- HOMEPAGE -->
+<url>
+<loc>{{ url('/') }}</loc>
+<lastmod>{{ now()->toAtomString() }}</lastmod>
+<changefreq>daily</changefreq>
+<priority>1.00</priority>
+</url>
+
+<!-- MAIN PAGES -->
+<url>
+<loc>{{ url('/about') }}</loc>
+<lastmod>{{ now()->toAtomString() }}</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.80</priority>
+</url>
+
+<url>
+<loc>{{ url('/contact') }}</loc>
+<lastmod>{{ now()->toAtomString() }}</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.80</priority>
+</url>
+
+<!-- CATEGORIES -->
+<url>
+<loc>{{ url('/tepiha') }}</loc>
+<lastmod>{{ now()->toAtomString() }}</lastmod>
+<changefreq>weekly</changefreq>
+<priority>0.95</priority>
+</url>
+
+<url>
+<loc>{{ url('/anesore') }}</loc>
+<lastmod>{{ now()->toAtomString() }}</lastmod>
+<changefreq>weekly</changefreq>
+<priority>0.92</priority>
+</url>
+
+<url>
+<loc>{{ url('/perde-ditore') }}</loc>
+<lastmod>{{ now()->toAtomString() }}</lastmod>
+<changefreq>weekly</changefreq>
+<priority>0.92</priority>
+</url>
+
+<url>
+<loc>{{ url('/postava') }}</loc>
+<lastmod>{{ now()->toAtomString() }}</lastmod>
+<changefreq>weekly</changefreq>
+<priority>0.90</priority>
+</url>
+
+<url>
+<loc>{{ url('/mbulesa') }}</loc>
+<lastmod>{{ now()->toAtomString() }}</lastmod>
+<changefreq>weekly</changefreq>
+<priority>0.90</priority>
+</url>
+
+<url>
+<loc>{{ url('/batanije') }}</loc>
+<lastmod>{{ now()->toAtomString() }}</lastmod>
+<changefreq>weekly</changefreq>
+<priority>0.88</priority>
+</url>
+
+<url>
+<loc>{{ url('/tepihebanjo') }}</loc>
+<lastmod>{{ now()->toAtomString() }}</lastmod>
+<changefreq>weekly</changefreq>
+<priority>0.88</priority>
+</url>
+
+<url>
+<loc>{{ url('/posteqia') }}</loc>
+<lastmod>{{ now()->toAtomString() }}</lastmod>
+<changefreq>weekly</changefreq>
+<priority>0.88</priority>
+</url>
+
+<url>
+<loc>{{ url('/garnishte') }}</loc>
+<lastmod>{{ now()->toAtomString() }}</lastmod>
+<changefreq>weekly</changefreq>
+<priority>0.88</priority>
+</url>
+
+<!-- 🔥 PRODUKTET AUTOMATIKE -->
+@foreach($products as $product)
+<url>
+<loc>{{ route('products.show', $product->slug) }}</loc>
+<lastmod>{{ $product->updated_at->toAtomString() }}</lastmod>
+<changefreq>weekly</changefreq>
+<priority>0.85</priority>
+</url>
+@endforeach
+
+</urlset>
