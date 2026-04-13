b473233b8fb30743c5194fe05cf30bbc3050f8f0
Lirie Bytyqi <leart.bytyqii98@gmail.com>
2026-04-14 00:08:17 +0200

    ...

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -763,6 +763,7 @@
           'name' => 'XK',
         ],
         'merchantReturnDays' => 14,
+        'returnPolicyCategory' => 'https://schema.org/MoneyBack',
         'returnMethod' => 'https://schema.org/ReturnByMail',
       ],
     ],
