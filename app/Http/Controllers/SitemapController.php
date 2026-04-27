ec2680801866954d96fa6e2cdd8cb734db6cde24
Lirie Bytyqi <leart.bytyqii98@gmail.com>
2026-04-28 01:16:49 +0200

    ...

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -6,8 +6,32 @@ use App\Models\Product;
 
 class SitemapController extends Controller
 {
+    private array $pages = [
+        ['path' => '/', 'changefreq' => 'daily', 'priority' => '1.00'],
+        ['path' => '/products', 'changefreq' => 'weekly', 'priority' => '0.90'],
+        ['path' => '/tepiha', 'changefreq' => 'weekly', 'priority' => '0.95'],
+        ['path' => '/anesore', 'changefreq' => 'weekly', 'priority' => '0.92'],
+        ['path' => '/perde-ditore', 'changefreq' => 'weekly', 'priority' => '0.92'],
+        ['path' => '/postava', 'changefreq' => 'weekly', 'priority' => '0.90'],
+        ['path' => '/mbulesa', 'changefreq' => 'weekly', 'priority' => '0.90'],
+        ['path' => '/jastekdekorues', 'changefreq' => 'weekly', 'priority' => '0.88'],
+        ['path' => '/batanije', 'changefreq' => 'weekly', 'priority' => '0.88'],
+        ['path' => '/tepihebanjo', 'changefreq' => 'weekly', 'priority' => '0.88'],
+        ['path' => '/posteqia', 'changefreq' => 'weekly', 'priority' => '0.88'],
+        ['path' => '/garnishte', 'changefreq' => 'weekly', 'priority' => '0.88'],
+        ['path' => '/search', 'changefreq' => 'weekly', 'priority' => '0.60'],
+        ['path' => '/track', 'changefreq' => 'weekly', 'priority' => '0.60'],
+        ['path' => '/cart', 'changefreq' => 'monthly', 'priority' => '0.40'],
+        ['path' => '/checkout', 'changefreq' => 'monthly', 'priority' => '0.40'],
+        ['path' => '/about', 'changefreq' => 'monthly', 'priority' => '0.80'],
+        ['path' => '/contact', 'changefreq' => 'monthly', 'priority' => '0.80'],
+        ['path' => '/terms', 'changefreq' => 'yearly', 'priority' => '0.50'],
+        ['path' => '/privacy', 'changefreq' => 'yearly', 'priority' => '0.50'],
+    ];
+
     public function index()
     {
+        $pages = $this->pages;
         $products = Product::query()
             ->where('is_active', 1)
             ->whereNotNull('slug')
@@ -16,7 +40,7 @@ class SitemapController extends Controller
             ->get(['slug', 'updated_at']);
 
         return response()
-            ->view('sitemap', compact('products'))
+            ->view('sitemap', compact('pages', 'products'))
             ->header('Content-Type', 'application/xml')
             ->header('X-Content-Type-Options', 'nosniff');
     }
--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,121 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-
-<!-- HOMEPAGE -->
+@foreach($pages as $page)
 <url>
-<loc>{{ url('/') }}</loc>
+<loc>{{ url($page['path']) }}</loc>
 <lastmod>{{ now()->toAtomString() }}</lastmod>
-<changefreq>daily</changefreq>
-<priority>1.00</priority>
+<changefreq>{{ $page['changefreq'] }}</changefreq>
+<priority>{{ $page['priority'] }}</priority>
 </url>
-
-<!-- MAIN PAGES -->
-<url>
-<loc>{{ url('/about') }}</loc>
-<lastmod>{{ now()->toAtomString() }}</lastmod>
-<changefreq>monthly</changefreq>
-<priority>0.80</priority>
-</url>
-
-<url>
-<loc>{{ url('/contact') }}</loc>
-<lastmod>{{ now()->toAtomString() }}</lastmod>
-<changefreq>monthly</changefreq>
-<priority>0.80</priority>
-</url>
-
-<url>
-<loc>{{ url('/terms') }}</loc>
-<lastmod>{{ now()->toAtomString() }}</lastmod>
-<changefreq>yearly</changefreq>
-<priority>0.50</priority>
-</url>
-
-<url>
-<loc>{{ url('/privacy') }}</loc>
-<lastmod>{{ now()->toAtomString() }}</lastmod>
-<changefreq>yearly</changefreq>
-<priority>0.50</priority>
-</url>
-
-<!-- CATEGORIES -->
-<url>
-<loc>{{ url('/products') }}</loc>
-<lastmod>{{ now()->toAtomString() }}</lastmod>
-<changefreq>weekly</changefreq>
-<priority>0.90</priority>
-</url>
-
-<url>
-<loc>{{ url('/tepiha') }}</loc>
-<lastmod>{{ now()->toAtomString() }}</lastmod>
-<changefreq>weekly</changefreq>
-<priority>0.95</priority>
-</url>
-
-<url>
-<loc>{{ url('/anesore') }}</loc>
-<lastmod>{{ now()->toAtomString() }}</lastmod>
-<changefreq>weekly</changefreq>
-<priority>0.92</priority>
-</url>
-
-<url>
-<loc>{{ url('/perde-ditore') }}</loc>
-<lastmod>{{ now()->toAtomString() }}</lastmod>
-<changefreq>weekly</changefreq>
-<priority>0.92</priority>
-</url>
-
-<url>
-<loc>{{ url('/postava') }}</loc>
-<lastmod>{{ now()->toAtomString() }}</lastmod>
-<changefreq>weekly</changefreq>
-<priority>0.90</priority>
-</url>
-
-<url>
-<loc>{{ url('/mbulesa') }}</loc>
-<lastmod>{{ now()->toAtomString() }}</lastmod>
-<changefreq>weekly</changefreq>
-<priority>0.90</priority>
-</url>
-
-<url>
-<loc>{{ url('/batanije') }}</loc>
-<lastmod>{{ now()->toAtomString() }}</lastmod>
-<changefreq>weekly</changefreq>
-<priority>0.88</priority>
-</url>
-
-<url>
-<loc>{{ url('/tepihebanjo') }}</loc>
-<lastmod>{{ now()->toAtomString() }}</lastmod>
-<changefreq>weekly</changefreq>
-<priority>0.88</priority>
-</url>
-
-<url>
-<loc>{{ url('/posteqia') }}</loc>
-<lastmod>{{ now()->toAtomString() }}</lastmod>
-<changefreq>weekly</changefreq>
-<priority>0.88</priority>
-</url>
-
-<url>
-<loc>{{ url('/jastekdekorues') }}</loc>
-<lastmod>{{ now()->toAtomString() }}</lastmod>
-<changefreq>weekly</changefreq>
-<priority>0.88</priority>
-</url>
-
-<url>
-<loc>{{ url('/garnishte') }}</loc>
-<lastmod>{{ now()->toAtomString() }}</lastmod>
-<changefreq>weekly</changefreq>
-<priority>0.88</priority>
-</url>
-
-<!-- 🔥 PRODUKTET AUTOMATIKE -->
+@endforeach
 @foreach($products as $product)
 <url>
 <loc>{{ url('/products/'.$product->slug) }}</loc>
@@ -124,5 +16,4 @@
 <priority>0.85</priority>
 </url>
 @endforeach
-
 </urlset>
